6ee9af384d2d374b34400ee2770c9a157e1bdaf2
wellington santos <weltossousa@gmail.com>
2019-03-27 17:43:14 -0300

    commit de teste

--- projetoOPE/app.js
+++ projetoOPE/app.js
@@ -2,8 +2,5 @@ let app = require('./config/server');
 let port = 8000
 
 app.listen(port, function(){
-	var dtNow = new Date;
-	console.log('------------------------------------------------');
-	console.log('    Servidor rodando na porta '+port+': '+dtNow.getHours() + ":" + dtNow.getMinutes() + ":" + dtNow.getSeconds());
-	console.log('------------------------------------------------');
+	console.log("servidor funcionando na porta "+port);
 });
