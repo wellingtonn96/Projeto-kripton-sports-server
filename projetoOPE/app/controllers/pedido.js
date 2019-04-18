fb76a3002f1e2e7f53566ffe98d4eab50667fafb
wellington santos <weltossousa@gmail.com>
2019-04-18 09:40:58 -0300

    consertando bug da pagina inicial

--- projetoOPE/app/controllers/pedido.js
+++ projetoOPE/app/controllers/pedido.js
@@ -6,7 +6,8 @@ exports.addPedido = (application, req, res)=>{
 		produtosModel.listarProduto().then(result => {
 			res.render("pedido/addPedido",{
                 usuarios: usuario,
-                produtos: result
+				produtos: result,
+				produto : {}
             });
 		}).catch(error => console.log(error))
 	} else {
@@ -17,5 +18,25 @@ exports.addPedido = (application, req, res)=>{
 }
 
 exports.pedido = (application, req, res)=>{
-	console.log(req.body)
+	if(req.session.autorizado){
+		const usuario = req.session.usuario
+		const connection = application.config.dbConnection();
+		const produtosModel = new application.app.models.ProdutosDao(connection);
+		const id  = req.body.idProduto
+		produtosModel.listarProduto().then(result => {
+			produtosModel.dadosProduto(id).then(produto=>{
+				const array = []
+				array.push(produto[0])
+				res.render("pedido/addPedido",{
+					usuarios: usuario,
+					produtos: result,
+					produto : array
+				});
+			}).catch(error => console.log(error))			
+		}).catch(error => console.log(error))
+
+	}else {
+		res.render("login/login", {validacao : {}});	
+	}
+	
 }
